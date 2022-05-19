ae273082544fcb42715a49602c7d00b3136c312a
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-05-19 19:19:00 +0000

    Make co-authors be included in commits and have a co-authors file     Co-authored-by: sigonasr2     Co-authored-by: <sigonasr2@gmail.com>

--- Java/scripts/commit.sh
+++ Java/scripts/commit.sh
@@ -1,6 +1,13 @@
 #Adds a commit message and pushes project to github repository.
 #Java
+AUTHORS=$(cat utils/.coauthors)
+COMMIT_MESSAGE="$*"
+for a in $AUTHORS
+do
+    COMMIT_MESSAGE+="
+    Co-authored-by: $a"
+done
 git add -u
 git add *
-git commit -m "$*"
+git commit -m "$COMMIT_MESSAGE"
 git push
--- scripts/commit.sh
+++ scripts/commit.sh
@@ -7,11 +7,18 @@ find . -type f -name md5 -delete
 
 #Generate a new hash for every sub-directory, which may require an update.
 FILES=$(cat utils/.updateDirectories) 
+AUTHORS=$(cat utils/.coauthors)
+COMMIT_MESSAGE="$*"
+for a in $AUTHORS
+do
+    COMMIT_MESSAGE+="
+    Co-authored-by: $a"
+done
 for f in $FILES
 do 
     search $f
 done
 git add -u
 git add *
-git commit -m "$*"
+git commit -m "$COMMIT_MESSAGE"
 git push
