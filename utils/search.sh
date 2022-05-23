1e9b5ce21696f3f0aeb5d33ea2e0bd66744f4601
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-05-23 19:58:32 +0000

    use a filelist system for updates, remove configurable files from md5
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- utils/search.sh
+++ utils/search.sh
@@ -8,11 +8,14 @@ function search() {
             search $1$g/
         else 
             echo "$1$g is a file"
-            if [ $g != "md5" ]; then
-                SUM=$(md5sum < $1$g)
-                echo "$g:$SUM" >> $1md5
+            if [ $g != "md5" ] && [ $g != "filelist" ]; then
+                if [ $g != ".coauthors" ] && [ $g != "version_info" ]; then
+                    SUM=$(md5sum < $1$g)
+                    echo "$g:$SUM" >> $1md5
+                fi
+                echo "$g" >> $1filelist
             else
-                echo "  md5 file, ignoring..."
+                echo "  ignoring $g..."
             fi
         fi
     done
@@ -35,7 +38,41 @@ function check() {
                 IFS=':' read -ra split <<< $line
                 g="${split[0]}"
                 echo "LINE -- $g" 
-                if [ "$g" != "md5" ]; then
+                if [ "$g" != "md5" ] && [ "$g" != "filelist" ]; then
+                    if [ -f $1$g ];
+                    then
+                        if [ "$g" != ".coauthors" ] && [ "$g" != "version_info" ]; then
+                            echo "++Redownload $1$g..."
+                            if [ -f "$1$g" ]; then
+                                curl -H 'Cache-Control: no-cache, no-store' "https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g" --output $1$g
+                            else
+                                echo "===Could not find directory, assuming regular scripts directory exists."
+                                curl -H 'Cache-Control: no-cache, no-store' "https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g" --output $LANGUAGE/scripts/$g
+                            fi
+                        fi
+                    else 
+                        echo "++==Downloading $1$g..."
+                        curl -H 'Cache-Control: no-cache, no-store' "https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g" --output $1$g
+                    fi
+                fi
+            done < /tmp/out
+        fi
+    fi
+    if [ -f "$1/filelist" ];
+    then
+        echo "   filelist: https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1filelist"
+        curl -H 'Cache-Control: no-cache, no-store' -s "https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1filelist" --output /tmp/out
+        cmp -s $1/filelist /tmp/out
+        if [ "$?" -ne 0 ] 
+        then
+            echo " Differences detected!"
+            cat /tmp/out 
+            while IFS= read -r line
+            do
+                IFS=':' read -ra split <<< $line
+                g="${split[0]}"
+                echo "LINE -- $g" 
+                if [ "$g" != "md5" ] && [ "$g" != "filelist" ]; then
                     if [ -f $1$g ];
                     then
                         if [ "$g" != ".coauthors" ] && [ "$g" != "version_info" ]; then
