5788bda0a0eb81c990a6a5dca7021ef7eaab2bf2
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-05-23 17:15:15 +0000

    Add a hint to see what's going on
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- utils/search.sh
+++ utils/search.sh
@@ -34,32 +34,33 @@ function check() {
             do
                 IFS=':' read -ra split <<< $line
                 g="${split[0]}"
+                echo "LINE -- $g"
                 if [ "$g" != "md5" ]; then
                     if [ -f $1$g ];
                     then
-			if [ "$g" != ".coauthors" ]; then
-                        echo "++Redownload $1$g..."
-                        if [ -f "$1$g" ]; then
-                            #Read the 2nd line and see if it has a special directory.
-                            CHECKLINE=$(sed -n '2{p;q;}' $1$g)
-                            if [ "${CHECKLINE:0:1}" = "#" ]; then
-                                #This could be a different diff, try that one.
-                                echo "   md5: https://raw.githubusercontent.com/sigonasr2/SigScript/main/${CHECKLINE:1}/$1md5"
-                                curl -H 'Cache-Control: no-cache, no-store' -s https://raw.githubusercontent.com/sigonasr2/SigScript/main/${CHECKLINE:1}/$1md5 --output /tmp/out
-                                cmp -s $1/md5 /tmp/out
-                                if [ "$?" -ne 0 ] 
-                                then
-                                    echo " Differences detected here too."
-                                    curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/sigonasr2/SigScript/main/${CHECKLINE:1}/$1$g --output $1$g
-                                fi
-                            else
-                                curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g --output $1$g
-                            fi
-                        else
-                            echo "===Could not find directory, assuming regular scripts directory exists."
-                            curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g --output scripts/$g
+                        if [ "$g" != ".coauthors" ]; then
+                                    echo "++Redownload $1$g..."
+                                    if [ -f "$1$g" ]; then
+                                        #Read the 2nd line and see if it has a special directory.
+                                        CHECKLINE=$(sed -n '2{p;q;}' $1$g)
+                                        if [ "${CHECKLINE:0:1}" = "#" ]; then
+                                            #This could be a different diff, try that one.
+                                            echo "   md5: https://raw.githubusercontent.com/sigonasr2/SigScript/main/${CHECKLINE:1}/$1md5"
+                                            curl -H 'Cache-Control: no-cache, no-store' -s https://raw.githubusercontent.com/sigonasr2/SigScript/main/${CHECKLINE:1}/$1md5 --output /tmp/out
+                                            cmp -s $1/md5 /tmp/out
+                                            if [ "$?" -ne 0 ] 
+                                            then
+                                                echo " Differences detected here too."
+                                                curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/sigonasr2/SigScript/main/${CHECKLINE:1}/$1$g --output $1$g
+                                            fi
+                                        else
+                                            curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g --output $1$g
+                                        fi
+                                    else
+                                        echo "===Could not find directory, assuming regular scripts directory exists."
+                                        curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g --output scripts/$g
+                                    fi
                         fi
-			fi
                     else 
                         echo "++==Downloading $1$g..."
                         curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/sigonasr2/SigScript/main/$1$g --output $1$g
