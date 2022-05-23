9a8fd43ff148a7f2cc13c5216aa805f298df8f92
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-05-23 17:47:35 +0000

    Don't move the jar file out, keep it in the bin directory
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- Java/scripts/jar.sh
+++ Java/scripts/jar.sh
@@ -11,6 +11,5 @@ cd ${OUT_DIR}
 jar cfm ${PROJECT_NAME}.jar ${ORIGINAL_LOC}/manifest sig
 printf "\n\n\nRunning Program...\n\n"
 java -jar ${PROJECT_NAME}.jar "$@"
-mv ${PROJECT_NAME}.jar ${ORIGINAL_LOC}
 cd ..
 ./${LANGUAGE}/scripts/clean.sh
