57eb15a658df61ba494581386b45ddcbc70acc30
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-06-06 14:35:52 +0000

    Create package system for Java projects
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

---
+++ Java/scripts/release.sh
@@ -0,0 +1,15 @@
+#Creates a zip file containing all project contents.
+#Java
+source ${LANGUAGE}/scripts/version_info
+TARGET_FILE="$PROJECT_NAME_$RELEASE_VERSION.zip"
+rm $TARGET_FILE
+FILES=$(cat ${LANGUAGE}/scripts/.package.files) 
+echo "Creating Package $TARGET_FILE..."
+for f in $FILES
+do 
+    zip -ur TARGET_FILE $f
+done
+echo "Complete!"
+echo ""
+echo ""
+echo ""
