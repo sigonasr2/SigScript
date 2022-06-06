7a1e2e8e84ed5ee3e1b873eea0eaf6799ec29f7b
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-06-06 14:46:53 +0000

    clean needs to be run inside of bin but needs to access the correct script
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- Java/scripts/jar.sh
+++ Java/scripts/jar.sh
@@ -13,5 +13,5 @@ jar cfm ${PROJECT_NAME}.jar ${ORIGINAL_LOC}/manifest sig
 jar uf ${PROJECT_NAME}.jar -C ../lib/bin/ .
 printf "\n\n\nRunning Program...\n\n"
 java -cp .:../lib/bin -Djava.library.path="${LIBRARY_PATH}" -jar ${PROJECT_NAME}.jar "$@"
-./${LANGUAGE}/scripts/clean.sh
+../${LANGUAGE}/scripts/clean.sh
 cd ..
