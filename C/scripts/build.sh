394bf7ea1d3944b3e374d638e0a94374be1bba88
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-29 17:48:11 +0000

    Change C build script to include custom params variable.
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- C/scripts/build.sh
+++ C/scripts/build.sh
@@ -1,5 +1,5 @@
 #Compiles the entire program then runs it, producing an executable.
 #C
-if gcc $(find . -type f -name "*.c") -lncurses -o ${PROJECT_NAME}; then
+if gcc $(find . -type f -name "*.c") ${CUSTOM_PARAMS} -o ${PROJECT_NAME}; then
     ./${PROJECT_NAME}
 fi
