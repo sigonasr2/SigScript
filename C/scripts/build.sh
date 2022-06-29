4e08e1055e01a186ddc8514bf1da4288562866c5
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-29 12:55:19 -0500

    Update build.sh

--- C/scripts/build.sh
+++ C/scripts/build.sh
@@ -1,5 +1,7 @@
 #Compiles the entire program then runs it, producing an executable.
 #C
+printf "Running program...\n\n\n"
 if gcc $(find . -type f -name "*.c") ${CUSTOM_PARAMS} -o ${PROJECT_NAME}; then
     ./${PROJECT_NAME}
 fi
+printf "\n\n"
