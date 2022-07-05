abf8eadd447e2adda303d5a9423f727042c177cf
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-07-05 12:32:31 +0000

    Include args when running a C program.
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- C/scripts/build.sh
+++ C/scripts/build.sh
@@ -2,6 +2,6 @@
 #C
 printf "Running program...\n\n\n"
 if gcc $(find . -type f -name "*.c") ${CUSTOM_PARAMS} -o ${PROJECT_NAME}; then
-    ./${PROJECT_NAME}
+    ./${PROJECT_NAME} "$@"
 fi
 printf "\n\n"
